ac92a9c65b30569e14bdd94ead6861c53a107c28
pankajbishnoi18 <pankajbishnoi2916@gmail.com>
2026-05-24 09:32:04 +0530

    added error handling in case api's dont respond

--- brain.py
+++ brain.py
@@ -9,15 +9,25 @@ def get_coordinates(city):
         "User-Agent":"weather app "
 
     }
-    response=requests.get(url,
+    try:
+
+        response=requests.get(url,
                            params=params,
                            headers=headers,timeout=5)
-    
-    data=response.json()
-    if data==[]:
-        return "sorry sir cooordinates of this city is not available thus i cant access weather update, try changing city or check if it is spelled correctly"
+        response.raise_for_status()
+        data=response.json()
+        if data==[]:
+           return "sorry sir cooordinates of this city is not available thus i cant access weather update, try changing city or check if it is spelled correctly"
         
-    return data[0]["lat"],data[0]["lon"]
+        return data[0]["lat"],data[0]["lon"]
+    except requests.exceptions.Timeout:
+       
+        return  ("request timed out(coordinate API)") 
+    except requests.exceptions.RequestException as e:
+        
+        return (f"coordinate API error: {e}")
+    
+    
 
 def weather_status(lat,lon):
     url="https://api.open-meteo.com/v1/forecast"
@@ -28,14 +38,20 @@ def weather_status(lat,lon):
 
     }
     
-    response=requests.get(url,params=params,timeout=5)
+    try :
+        response=requests.get(url,params=params,timeout=5)
+        response.raise_for_status()
     
-    data=response.json()
-    return data
+        data=response.json()
+        return data
+    except requests.exceptions.Timeout:
+        return "request timed out(weather API)"
+    except requests.exceptions.RequestExcepton as e:
+        return f"weather API error: {e}"
 def fetch_weather(city):
     coord=get_coordinates(city)
-    if coord=="sorry sir cooordinates of this city is not available thus i cant access weather update, try changing city or check if it is spelled correctly":
-        return "sorry sir cooordinates of this city is not available thus i cant access weather update, try changing city or check if it is spelled correctly"
+    if not isinstance(coord,tuple):
+        return coord
 
     lat=coord[0]
     lon=coord[1]
